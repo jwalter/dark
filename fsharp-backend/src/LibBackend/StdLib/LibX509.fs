025ba9aa267f0328e12b5af40e11a81ae29196e7
Joakim Waltersson <joakim.waltersson@gmail.com>
2021-04-04 13:31:54 +0200

    WIP

--- fsharp-backend/src/LibBackend/StdLib/LibX509.fs
+++ fsharp-backend/src/LibBackend/StdLib/LibX509.fs
@@ -1,7 +1,11 @@
 module LibBackend.StdLib.LibX509
 
+open System
 open System.Threading.Tasks
 open System.Numerics
+open System.Text
+open System.Security.Cryptography
+open System.Security.Cryptography.X509Certificates
 open FSharp.Control.Tasks
 open FSharpPlus
 
@@ -19,32 +23,35 @@ let incorrectArgs = LibExecution.Errors.incorrectArgs
 let varA = TVariable "a"
 let varB = TVariable "b"
 
-let fns : List<BuiltInFn> = []
-  // [ { name = fn "X509" "pemCertificatePublicKey" 0
-  //
-  //   ; parameters = [Param.make "pemCert" TStr ""]
-  //   ; returnType = TResult
-  //   ; description =
-  //       "Extract the public key from a PEM encoded certificate and return the key in PEM format."
-  //   ; fn =
-  //         (function
-  //         | _, [DStr cert] ->
-  //           ( try
-  //               cert
-  //               |> U.to_string
-  //               |> Cstruct.of_string
-  //               |> X509.Encoding.Pem.Certificate.of_pem_cstruct1
-  //               |> X509.public_key
-  //               |> X509.Encoding.Pem.Public_key.to_pem_cstruct1
-  //               |> Cstruct.to_string
-  //               |> DStr
-  //               |> ResOk
-  //               |> DResult
-  //             with Invalid_argument msg ->
-  //               DResult (ResError (DStr msg)) )
-  //         | _ ->
-  //             incorrectArgs ())
-  //   ; sqlSpec = NotYetImplementedTODO
-  //   ; previewable = Impure
-  //   ; deprecated = NotDeprecated } ]
-  //
+let fns : List<BuiltInFn> =
+  [ { name = fn "X509" "pemCertificatePublicKey" 0
+ 
+    ; parameters = [Param.make "pemCert" TStr ""]
+    ; returnType = TResult(TStr, TStr)
+    ; description =
+        "Extract the public key from a PEM encoded certificate and return the key in PEM format."
+    ; fn =
+          (function
+          | _, [DStr cert] ->
+            ( try
+                
+                let mutable span = new ReadOnlySpan<char>(cert.ToCharArray())
+                let fields = PemEncoding.Find span
+                let label = fields.Base64Data
+                let range = span.Slice(label.Start.Value, label.End.Value).ToString()
+                let pem = new X509Certificate2(Convert.FromBase64String(range))
+                let mutable pubLabel = "PUBLIC_KEY".AsSpan()
+                let pemData = new ReadOnlySpan<byte>(pem.PublicKey.EncodedKeyValue.RawData)
+                let pubPem = PemEncoding.Write(pubLabel, pemData)
+                pubPem
+                |> String
+                |> DStr
+                |> Ok
+                |> DResult
+                |> Value
+              with e -> Value(DResult(Error(DStr e.Message))))
+          | _ -> incorrectArgs ())
+    ; sqlSpec = NotYetImplementedTODO
+    ; previewable = Impure
+    ; deprecated = NotDeprecated } ]
+ 
